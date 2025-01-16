51adbc9b7d7ca5baceb55c49caf754e8830e7cae
Aloy <aloyleow91@gmail.com>
2025-01-16 13:38:08 +0800

    feat: integrated backend and frontend useitems

--- src/components/gameScreen/humanDashboard/InventoryComponent.tsx
+++ src/components/gameScreen/humanDashboard/InventoryComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import inventoryItems from "../../../services/inventoryServices";
+import { inventoryItems, applyItemStats } from "../../../services/inventoryServices";
 
 type Item = {
   numPath: number;
@@ -15,6 +15,39 @@ type Item = {
     maxmana: number;
   }
 };
+
+type PlayeritemsData = {
+  item: {
+    numPath: number;
+    name: string;
+    role: string;
+    cost: number;
+    description: string;
+    properties: {
+      attack: number;
+      health: number;
+      mana: number;
+      maxhealth: number;
+      maxmana: number;
+    }
+  },
+  items: string[];
+  role: string;
+  attack: number;
+  maxhealth: number;
+  maxmana: number;
+  health: number;
+  mana: number;
+};
+
+type PlayerNewData = {
+  items: string[];
+  attack: number;
+  maxhealth: number;
+  maxmana: number;
+  health: number;
+  mana: number;
+}
 
 type InventoryComponentProps = {
   setOpenInventory: React.Dispatch<React.SetStateAction<boolean>>;
@@ -48,42 +81,43 @@ const InventoryComponent: React.FC<InventoryComponentProps> = ({ setOpenInventor
     };
 
     showItemsInInventory();
-  },[player])
+  }, [player])
 
-  // const handleOnUse = (item: Item) => {
-  //   const totalMH = player.maxhealth + item.properties.maxhealth;
-  //   const totalMM = player.maxmana + item.properties.maxmana;
-  //   const adjustedHealth = player.health + item.properties.health;
-  //   const adjustedMana = player.maxmana + item.properties.maxmana;
-  //   const adjustedAttack = player.attack + item.properties.attack;
+  const handleOnUse = async (item: Item) => {
 
-  //   const filterItems = (prevItems: PlayerType) => {
-  //     const inventoryItemIdx = prevItems.items.findIndex((playerItems) => playerItems === item.name)
-  //     if (inventoryItemIdx !== -1){
-  //       return [...prevItems.items.slice(0, inventoryItemIdx), ...prevItems.items.slice(inventoryItemIdx + 1)];
-  //     }
-  //     return prevItems.items;
-  //   };
+    const playerItemData: PlayeritemsData = {
+      item: item,
+      items: player.items,
+      role: player.role,
+      attack: player.attack,
+      maxhealth: player.maxhealth,
+      maxmana: player.maxmana,
+      health: player.health,
+      mana: player.mana,
+    }
+    try {
+      const playerDataRes: PlayerNewData = await applyItemStats(playerItemData)
+      setPlayer({
+        ...player,
+        items: playerDataRes.items,
+        health: playerDataRes.health,
+        mana: playerDataRes.mana,
+        maxhealth: playerDataRes.maxhealth,
+        maxmana: playerDataRes.maxmana,
+        attack: playerDataRes.attack
+      })
 
-  //   const filterInventory = (inv: ItemsResponse) => {
-  //     const inventoryItemIdx = inv.findIndex((invItem) => invItem.name === item.name)
-  //     if (inventoryItemIdx !== -1){
-  //       return [...inv.slice(0, inventoryItemIdx), ...inv.slice(inventoryItemIdx + 1)];
-  //     }
-  //     return inv;
-  //   };
+    } catch (error) {
 
-  //   setPlayer({
-  //     ...player,
-  //     health: adjustedHealth > totalMH ? player.maxhealth : adjustedHealth,
-  //     maxhealth: totalMH,
-  //     mana: adjustedMana > totalMM ? player.maxmana : adjustedMana,
-  //     maxmana: totalMM,
-  //     attack: adjustedAttack,
-  //     items: filterItems(player)
-  //   })
-  //   setInventory(filterInventory(inventory))
-  // }
+      if (error instanceof TypeError) {
+        console.error("Server Error, please contact admin");
+      } else if (error instanceof Error) {
+        console.error(`${error.message}`);
+      } else {
+        console.error("Client Error");
+      }
+    }
+  };
 
   return (
     <div className="gameToastDiv">
@@ -96,7 +130,7 @@ const InventoryComponent: React.FC<InventoryComponentProps> = ({ setOpenInventor
             <p>{item.name}</p>
             <p>{item.cost}g</p>
             <p>-{item.description}</p>
-            <button>Use</button>
+            <button onClick={() => handleOnUse(item)}>Use</button>
           </div>
         ))}
       </div>
--- src/services/inventoryServices.ts
+++ src/services/inventoryServices.ts
@@ -2,6 +2,39 @@ type PlayerItems = {
   items: string[]
 };
 
+type PlayeritemsData = {
+  item: {
+    numPath: number;
+    name: string;
+    role: string;
+    cost: number;
+    description: string;
+    properties: {
+      attack: number;
+      health: number;
+      mana: number;
+      maxhealth: number;
+      maxmana: number;
+    }
+  },
+  items: string[];
+  role: string;
+  attack: number;
+  maxhealth: number;
+  maxmana: number;
+  health: number;
+  mana: number;
+};
+
+type PlayerNewData = {
+  items: string[];
+  attack: number;
+  maxhealth: number;
+  maxmana: number;
+  health: number;
+  mana: number;
+}
+
 const inventoryItems = async (playerItems: PlayerItems): Promise<ItemsResponse> => {
   try {
 
@@ -37,4 +70,39 @@ const inventoryItems = async (playerItems: PlayerItems): Promise<ItemsResponse> 
   }
 }
 
-export default inventoryItems
+const applyItemStats = async (playerItemsData: PlayeritemsData): Promise<PlayerNewData> => {
+  try {
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+      throw new Error("Authorization token error.");
+    }
+
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/verified/useitems`, {
+
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(playerItemsData),
+
+    });
+
+    const json = await res.json();
+    if (json.error) {
+      throw new Error(json.error);
+    }
+   
+    return json as PlayerNewData;
+    
+  } catch (error) {
+
+    console.error(error);
+    
+    throw error;
+
+  }
+}
+
+export { inventoryItems, applyItemStats }
